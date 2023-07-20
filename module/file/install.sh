69811ec77927b40b423d08c7397a38483d90cbf1
Yakhya Dabo <yakhyadabo@gmail.com>
2023-07-20 16:59:17 +0100

    [module] jenkins installation

--- module/file/install.sh
+++ module/file/install.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 echo "Preparing installation ..."
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt update -qq
 
 # Install JRE
 echo "Installing JRE ..."
 sudo apt install -y default-jre
+
+# Install and start Jenkins
+echo "Installing and start jenkins ..."
+sudo apt install -y jenkins
+sudo systemctl start jenkins
 
 # Install git
 echo "Installing git ..."
@@ -18,22 +25,16 @@ echo "Setting up SSH key ..."
 JENKINS_HOME=/var/lib/jenkins
 mkdir $JENKINS_HOME/.ssh
 touch $JENKINS_HOME/.ssh/known_hosts
-ssh-keygen-rsa -b 2048 -f /var/lib/jenkins/.ssh/id_rsa -N ''
+ssh-keygen -t rsa -b 2048 -f $JENKINS_HOME/.ssh/id_rsa -N ''
 chmod 700 $JENKINS_HOME/.ssh
 chmod 644 $JENKINS_HOME/.ssh/id_rsa.pub
 chmod 600 $JENKINS_HOME/.ssh/id_rsa
-chown-R jenkins jenkins $JENKINS_HOME/.ssh
+chown -R jenkins:jenkins $JENKINS_HOME/.ssh
 
 echo "Install Docker engine"
-sudo apt install docker.io
-usermod -a G docker ubuntu
-sudo systemctl enable --now docker
-docker --version
-
-# Install and start Jenkins
-echo "Installing and start jenkins ..."
-sudo apt install -y jenkins
-sudo systemctl start jenkins
+sudo apt install -y docker.io
+usermod -aG docker ubuntu
+systemctl start docker
 
 
 #sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
